4fc62b42811677569f2c154da7ba665598fb5402
zJuarez <mji2000.741@gmail.com>
2022-01-20 09:40:22 -0600

    Lab06 end

--- Lab06/js/index.js
+++ Lab06/js/index.js
@@ -1,13 +1,37 @@
 const agregar = document.getElementById('agregar');
+
 agregar.onclick = function(event) {
   event.preventDefault();
-  const text = document.getElementById("newText");
+  const text = document.getElementById('newText');
   const value = text.value;
-  var ul = document.getElementById("list");
-  var li = document.createElement("li");
-  li.appendChild(document.createTextNode(value));
+  const ul = document.getElementById('list');
+  const li = document.createElement('div')
+  li.setAttribute('class', 'li');
+  const h3 = document.createElement('h3');
+  const check = document.createElement('div')
+  check.setAttribute('class', 'button check');
+  const del = document.createElement('div')
+  del.setAttribute('class', 'button dele');
+
+  check.innerHTML = 'check'
+  del.innerHTML = 'delete'
+  h3.innerHTML = value;
+
+  li.appendChild(h3);
+  li.appendChild(check);
+  li.appendChild(del);
   ul.appendChild(li);
-  text.value = ""
+
+  check.onclick =
+      function(event) {
+    event.preventDefault;
+    h3.classList.toggle('checked')
+  }
+
+      del.onclick = function(event) {
+    event.preventDefault;
+    ul.removeChild(li);
+  } 
+  text.value = ''
   console.log(value);
-  
 }
